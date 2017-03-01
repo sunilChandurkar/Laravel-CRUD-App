b7056d30ba4098fdc0117199d2fada19a70b0474
Sunil <sunil_chandurkar@yahoo.com>
2017-02-28 22:00:18 -0500

    refactored

--- app/Http/Controllers/MenuItemController.php
+++ app/Http/Controllers/MenuItemController.php
@@ -81,7 +81,17 @@ class MenuItemController extends Controller
         //get the restaurant
         $restaurant = $this->item->findRestaurant($restaurant_id);
         //get the menuitem
-        $menuitem = $this->item->find($menu_id);        
+        $menuitem = $this->item->find($menu_id);  
+        //get the creator's name
+        $created_by = $this->get_menuitem_creators_name($menu_id);
+
+        //if the user did not create the menu item
+        if(!$this->match_user_ids($menu_id)){
+        \Session::flash('message', 'This MenuItem was created by ' . $created_by . 
+            '. You can only delete a MenuItem you have created.');
+            return back();
+        }            
+              
         return view('/menuitems/edit')->with(['menuitem'=>$menuitem, 'restaurant'=>$restaurant]);
     }
 
@@ -108,6 +118,16 @@ class MenuItemController extends Controller
         $menuitem = $this->item->find($menu_id);  
         //get the restaurant
         $restaurant = $this->item->findRestaurant($restaurant_id);
+        //get the creator's name
+        $created_by = $this->get_menuitem_creators_name($menu_id);
+
+        //if the user did not create the menu item
+        if(!$this->match_user_ids($menu_id)){
+        \Session::flash('message', 'This MenuItem was created by ' . $created_by . 
+            '. You can only delete a MenuItem you have created.');
+            return back();
+        }
+
         return view('/menuitems/delete', ['menuitem'=>$menuitem, 'restaurant'=>$restaurant]);
     }
 
@@ -140,4 +160,48 @@ class MenuItemController extends Controller
         'item-description'=> 'required|max:500'
             ]);
     }
+
+    /**
+     * This function matches the User ID of the logged In User
+     * with the USer ID of the User who has created the MenuItem.
+     * @param int $id id of the restaurant
+     * @return bool - true if the ids match, false otherwise
+     */
+    public function match_user_ids($menu_id){
+
+        //get the user_id of the logged in user
+        $logged_in_user_id = Auth::id(); 
+
+        //find the menu item with the given menu_id
+        $menuitem = $this->item->find($menu_id);
+
+        //get the user_id of the user who created the menuitem
+        $menuitem_creators_user_id = $menuitem->user_id;
+
+        //match the User ID of the logged In User
+        //with the USer ID of the User who has created the menuitem
+        if($logged_in_user_id==$menuitem_creators_user_id){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Get MenuItem Creator's Name
+     * @param int $menu_id id of the menuitem
+     * @return string - MenuItem Creator's Name
+     */
+    public function get_menuitem_creators_name($menu_id){
+
+        //find the menuitem with the given id
+        $menuitem = $this->item->find($menu_id);
+
+        //find the User name of the restaurant creator
+        $created_by = $menuitem->user->name;
+        
+        return $created_by;
+    }
+
 }
